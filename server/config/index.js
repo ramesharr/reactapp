871f42d627bd499724fb6bf2000cee8b48a2384c
ramesharr <ramesh1arr@gmail.com>
2018-07-08 09:55:33 +0530

    production

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'production',
   host: process.env.HOST || '0.0.0.0',
   port: process.env.PORT || 5000,
   jwtSecret: process.env.JWT_SECRET || 'chatting_app',
